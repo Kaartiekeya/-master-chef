c237146494ef31f0a978c22da914c89cdd368131
Kaartiekeya <82946406+Kaartiekeya@users.noreply.github.com>
2021-06-21 15:58:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+Maggie noodles
+
+ingredents:
+maagie
+bowl
+masala(given in the packet)
+water
+
+
+first take a bowl and cut the paket and keep maagie in the bowl.
+add maagie basala in to into it.after adding add some water and heat it for 5 minutes.now maagie is done
